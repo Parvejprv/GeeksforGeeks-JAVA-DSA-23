b8fbf624bd0aae41886176380860a9e02a879ac0
Parvej Alam <parvejprv1@gmail.com>
2024-03-18 11:21:13 +0530

    Segregate0and1

---
+++ GFG Solution/segregate0and1/segregate0and1.cpp
@@ -0,0 +1,22 @@
+void segregate0and1(int[] arr, int n)
+{
+  int count0 = 0;
+  int count1 = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    if (arr[i] == 0)
+      count0++;
+    else
+      count1++;
+  }
+
+  for (int i = 0; i < count0; i++)
+  {
+    arr[i] = 0;
+  }
+  for (int i = count0; i < n; i++)
+  {
+    arr[i] = 1;
+  }
+}
